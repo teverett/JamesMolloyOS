8b11f36881a086e3cab2985d5a5e7e58f2d0dbfb
Tom Everett <tom@khubla.com>
2025-04-15 16:38:18 -0600

    wrong compiler

--- kernel/mk/gcc-i386-darwin.mk
+++ kernel/mk/gcc-i386-darwin.mk
@@ -1,7 +1,7 @@
 # requires 'brew install x86_64-elf-gcc'
 
-GCC=x86_64-elf-gcc
-LD=x86_64-elf-ld
+GCC=i686-elf-gcc
+LD=i686-elf-ld
 CFLAGS=-c -ggdb3 -Wextra -Wno-sign-compare -Wno-implicit-function-declaration -fno-builtin -fno-stack-protector -m32 -nostdlib -nostdinc -std=gnu99
 
 LDFLAGS=-T src/link.ld -m elf_i386
@@ -9,17 +9,17 @@ NASM=nasm
 ASFLAGS=-felf
 
 # objcopy
-OBJCOPY=x86_64-elf-objcopy
+OBJCOPY=i686-elf-objcopy
 OBJCOPYFLAGS=-O binary
 
 # strip
-STRIP=x86_64-elf-strip
+STRIP=i686-elf-strip
 STRIPFLAGS=
 
 # ar
-AR=x86_64-elf-ar
+AR=i686-elf-ar
 ARFLAGS=-crs
 
 # qemu
-QEMU=qemu-system-x86_64
+QEMU=qemu-system-i686
 
